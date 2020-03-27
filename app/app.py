460122e16763a4db642dc9716ea435dbe745ac1e
Tomoyuki Hirose <still0529@gmail.com>
2020-03-27 19:49:58 +0900

    ルーティングの設定

--- app/app.py
+++ app/app.py
@@ -1,1 +1,20 @@
-#アプリのフロント側の処理（フォームとか、サーバ系とか）を書くファイル
+#サーバー側の処理を書くファイル
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/Usage')
+def Usage():
+    return render_template('usage.html')
+
+@app.route('/detection')
+def detection():
+    return render_template('detection.html')
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
