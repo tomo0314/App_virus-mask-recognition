b19479693ac28dbd213a896a6b66f52bb2c63d46
MasaKusuda <masa.kusuda.1999@gmail.com>
2020-03-29 10:26:45 +0900

    change

--- app/initializer.py
+++ app/initializer.py
@@ -1,16 +1,23 @@
 #initializer
+import tensorflow as tf
+import os
 import warnings
 warnings.filterwarnings('ignore')
-import tensorflow as tf
-import sys
 
-import os
 from os.path import join
 os.chdir('./Preprocessed') 
 path = os.getcwd()
 
 from keras.models import Sequential, model_from_json
+import keras
+graph = tf.get_default_graph()
+
+session = keras.backend.get_session()
+init = tf.global_variables_initializer()
+session.run(init)
+
 model = model_from_json(open(join(path, 'mask_NN.json'), 'r').read())
 model.load_weights(join(path,'mask_NN_weight.h5'))
+model._make_predict_function()
 
 os.chdir('./..')
--- app/mask.py
+++ app/mask.py
@@ -20,30 +20,32 @@ classes_dict = {"no-mask_data":0,
 classes = ["no-mask", "mask"]
 img_width, img_height = 128, 128
 
+
 def detect_mask(img_path):
-  model = initializer.model
-  img=Image.open(img_path)
-  img = img.resize((img_width, img_height))
-  img = np.array(img)
-  #検査対象画像の出力
-  #plt.imshow(img)
-  img=img.reshape([1,128,128,3])
-  pred=model.predict(img)
-  output = f"マスクを装着している確率{pred[0][1]*100}%\n"
-  if pred.argmax() == 1:
-    output+="マスクを装着しています"
-  else:
-    output+="マスクを装着していません"
-  return output
+  with initializer.graph.as_default():
+    model = initializer.model
+    img=Image.open(img_path)
+    img = img.resize((img_width, img_height))
+    img = np.array(img)
+    #検査対象画像の出力
+    #plt.imshow(img)
+    img=img.reshape([1,128,128,3])
+    pred=model.predict(img)
+    output = f"マスクを装着している確率{pred[0][1]*100}%\n"
+    if pred.argmax() == 1:
+
+      output+="マスクを装着しています"
+    else:
+      output+="マスクを装着していません"
+    return output
 
 
 def mask_test(file_name):
     os.chdir('./Testdata')
     path = os.getcwd()
     path_x = join(path,file_name)
+    print(path_x)
     #outputに結果として表示して欲しい文字列を格納しています
     output=detect_mask(path_x)
     print(output)
     os.chdir('./..')
-    return output
-
